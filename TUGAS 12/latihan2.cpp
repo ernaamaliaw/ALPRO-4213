32d030a72549990e2ff2df27da5a783015764bc6
ernaamaliaw <127821284+ernaamaliaw@users.noreply.github.com>
2023-06-27 15:56:58 +0700

    Add files via upload

---
+++ TUGAS 12/latihan2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+struct Sepeda {
+    string Merk;
+    string Type;
+    int Tahun;
+    string Harga;
+};
+
+int main()
+{
+    Sepeda sepeda;
+    sepeda.Merk = "Polygon";
+    sepeda.Type = "Sepeda Gunung";
+    sepeda.Tahun = 2013;
+    sepeda.Harga = "2.000.000";
+
+    cout << "Informasi Sepeda :" << endl;
+    cout << "Merk  : " << sepeda.Merk << endl;
+    cout << "Type  : " << sepeda.Type << endl;
+    cout << "Tahun : " << sepeda.Tahun << endl;
+    cout << "Harga : " << sepeda.Harga << endl;
+
+    return 0;
+}
+
